e8858aa3a30e34f7454f980998dbf448888ca1b8
j.mir <j.mir.prazak@gmail.com>
2022-08-01 17:26:42 +0200

    cache for nvidia

--- APP/nvidia.sh
+++ APP/nvidia.sh
@@ -2,24 +2,49 @@
 
 echo "INSTALLING NVIDIA AND DAVINCI RESOLVE"
 
-pacman -Syyu --noconfirm fuse3
+pacman -Syyu --noconfirm fuse3 glu librsvg libxcrypt-compat
 
 modprobe fuse
 
-cd /home/$HOSTUSER
-#sudo -u $HOSTUSER mkdir -p /home/$HOSTUSER/yay
-#cd /home/$HOSTUSER/yay
+sudo -u $HOSTUSER mkdir -p /tmp/build/
 
-sudo -u $HOSTUSER yay --noconfirm -S opencl-nvidia --overwrite '*'
+cd /tmp/build/
+if [ ! -f /tmp/build/yay/*zst ]; then 
+    sudo -u $HOSTUSER git clone https://aur.archlinux.org/yay.git ./yay
+    cd yay
+    sudo -u $HOSTUSER makepkg --noconfirm -sC  
+fi
+cd /tmp/build/yay
+pacman -U *zst --noconfirm
 
-curl https://us.download.nvidia.com/XFree86/Linux-${ARCH}/${NVIDIA_VERSION}/NVIDIA-Linux-${ARCH}-${NVIDIA_VERSION}.run --output nvidia.run
+
+cd /tmp/build/
+if [ ! -f /tmp/build/opencl-nvidia/*zst ]; then 
+    sudo -u $HOSTUSER yay --noconfirm -S opencl-nvidia --overwrite '*' --builddir=/tmp/build/
+else
+    cd /tmp/build/opencl-nvidia
+    pacman -U *zst --noconfirm --overwrite '*'
+fi
+
+
+cd /tmp/build/
+if [ ! -f /tmp/build/nvidia-drivers/*run ]; then
+    mkdir -p /tmp/build/nvidia-drivers
+    curl https://us.download.nvidia.com/XFree86/Linux-${ARCH}/${NVIDIA_VERSION}/NVIDIA-Linux-${ARCH}-${NVIDIA_VERSION}.run --output nvidia.run
+fi
+cd /tmp/build/nvidia-drivers/
 chmod +x ./nvidia.run
+bash /tmp/build/nvidia-drivers/nvidia.run --no-kernel-module --no-kernel-module-source --run-nvidia-xconfig --no-backup --no-questions --accept-license --ui=none
 
-bash ./nvidia.run --no-kernel-module --no-kernel-module-source --run-nvidia-xconfig --no-backup --no-questions --accept-license --ui=none
 
-sudo -u $HOSTUSER yay --noconfirm -S davinci-resolve-studio --overwrite '*'
+cd /tmp/build/
+if [ ! -f /tmp/build/davinci-resolve/*zst ]; then
+    sudo -u $HOSTUSER yay --noconfirm -S davinci-resolve --overwrite '*' --builddir=/tmp/build/
+else
+    cd /tmp/build/davinci-resolve/
+    pacman -U *zst --noconfirm --overwrite '*'
+fi
 
-rm /home/$HOSTUSER/* -rf
 
 
 #sudo -u arch yay --noconfirm -S davinci-resolve
