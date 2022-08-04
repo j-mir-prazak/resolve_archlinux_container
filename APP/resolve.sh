90b6d0d6617c4f720d0ec62a0bff28f7477729d9
j_mir_ <j.mir.prazak@gmail.com>
2022-08-04 11:25:03 +0200

    gitignore

--- APP/resolve.sh
+++ APP/resolve.sh
@@ -8,5 +8,5 @@ usermod -u $HOSTUSERID $HOSTUSER
 usermod -aG wheel $HOSTUSER
 usermod -aG video,audio,systemd-timesync,systemd-network,systemd-resolve,power,network,log,mail,bin,daemon,dbus,render,rfkill,rtkit,root $HOSTUSER
 
-sudo -u $HOSTUSER PULSE_SERVER=/var/run/user/$HOSTUSERID/pulse/native /opt/resolve/bin/resolve
+sudo -u $HOSTUSER PULSE_SERVER=/var/run/user/$HOSTUSERID/pulse/native tracefile /opt/resolve/bin/resolve
 #/bin/bash
